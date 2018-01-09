a49c6e8c5bce54722da856716be2d3785bfc886f
John <Xohnjoseph@gmail.com>
2018-01-10 04:14:36 +0700

    add babel polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path=require('path');
 
 module.exports={
-  entry: './src/app.js',
+  entry: ["babel-polyfill",'./src/app.js'],
   output:{
     path: path.join(__dirname,'public'),
     filename: 'bundle.js'
